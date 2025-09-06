98e7df08f1208e63e6ffedb7cf3f42dcfaf379b6
jonathan <yonilevi0013@gmail.com>
2025-09-06 15:28:25 +0300

    wip

--- main.tf
+++ main.tf
@@ -5,41 +5,42 @@ module "vpc_creation" {
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-module "ec2_creation" {
-  # count           = var.ec2_module_creation ? 1 : 0
-  source          = "./modules/ec2"
-  private_subnets = module.vpc_creation.private_subnet_ids
-  public_subnets  = module.vpc_creation.public_subnet_ids
-  vpc_id          = module.vpc_creation.vpc_id
-  user_data       = <<-EOT
-    #!/bin/bash
-    yum update -y
-    yum install -y stress -y
-    # Run stress for 5 minutes with 2 CPU workers
-    stress --cpu 2 --timeout 1200
-  EOT
-  ec2_count       = 3
-  depends_on      = [module.vpc_creation]
-}
+# module "ec2_creation" {
+#   # count           = var.ec2_module_creation ? 1 : 0
+#   source          = "./modules/ec2"
+#   private_subnets = module.vpc_creation.private_subnet_ids
+#   public_subnets  = module.vpc_creation.public_subnet_ids
+#   vpc_id          = module.vpc_creation.vpc_id
+#   user_data       = <<-EOT
+#     #!/bin/bash
+#     yum update -y
+#     yum install -y stress -y
+#     # Run stress for 5 minutes with 2 CPU workers
+#     stress --cpu 2 --timeout 1200
+#   EOT
+#   ec2_count       = 3
+#   depends_on      = [module.vpc_creation]
+# }
 
-module "s3" {
-  # count  = var.s3_module_creation ? 1 : 0
-  source = "./modules/s3"
-  region = var.region
-}
+# module "s3" {
+#   # count  = var.s3_module_creation ? 1 : 0
+#   source = "./modules/s3"
+#   region = var.region
+# }
 
-module "cloudwatch" {
-  # count           = var.cloudwatch_module_creation ? 1 : 0
-  source          = "./modules/cloudwatch"
-  region          = var.region
-  ec2_instance_id = module.ec2_creation.ec2_instance_id
-  depends_on      = [module.ec2_creation]
-}
+# module "cloudwatch" {
+#   # count           = var.cloudwatch_module_creation ? 1 : 0
+#   source          = "./modules/cloudwatch"
+#   region          = var.region
+#   ec2_instance_id = module.ec2_creation.ec2_instance_id
+#   depends_on      = [module.ec2_creation]
+# }
 
-module "codepipeline" {
-  source              = "./modules/codepipeline"
-  codepipeline-region = var.region
-}
+# module "codepipeline" {
+#   source              = "./modules/codepipeline"
+#   codepipeline-region = var.region
+#   codepipeline-name   = "test-pipeline01"
+# }
 
 module "ecr_creation" {
   source     = "./modules/ecr"
